a5bc99645ce9a2ee5847c0e39978ec03622891c4
Muhammad Rizky Amin <muhammad.amin@MIRACLE-GROUP.LOCAL>
2025-06-24 17:46:43 +0700

    tes2

--- app/Imports/AssignmentsImport.php
+++ app/Imports/AssignmentsImport.php
@@ -5,7 +5,6 @@ namespace App\Imports;
 use App\Models\Assignments;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
-use Carbon\Carbon;
 
 class AssignmentsImport implements ToModel, WithHeadingRow
 {
@@ -20,26 +19,12 @@ class AssignmentsImport implements ToModel, WithHeadingRow
         return new Assignments([
             'asset_id'        => $row['asset_id'],
             'user_id'         => $row['user_id'],
-            'assignment_date' => $this->convertDate($row['assignment_date']),
-            'return_date'     => $this->convertDate($row['return_date']),
+            'assignment_date' => $row['assignment_date'],
+            'return_date'     => $row['return_date'],
             'condition_note'  => $row['condition_note'],
             'received_by'     => $row['received_by'],
             'status'          => $row['status'],
             'document_url'    => $row['document_url'],
         ]);
     }
-
-    private function convertDate($value)
-{
-    if (!$value || strtolower($value) == 'n/a') {
-        return null;
-    }
-
-    // Jika nilai tanggal pakai format d/m/Y (seperti dari Excel), convert ke Y-m-d
-    try {
-        return Carbon::createFromFormat('d/m/Y', $value)->format('Y-m-d');
-    } catch (\Exception $e) {
-        return $value; // fallback jika format sudah benar
-    }
-}
 }
